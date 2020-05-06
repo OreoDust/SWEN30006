65fda66800e0738a72925d937f83d8b55695aa2d
haoqic <39285061+OreoDust@users.noreply.github.com>
2020-05-06 16:44:04 +1000

    Create Building.java

---
+++ PROJ01/src/automail/Building.java
@@ -0,0 +1,15 @@
+package automail;
+
+public class Building {
+	
+	
+    /** The number of floors in the building **/
+    public static int FLOORS;
+    
+    /** Represents the ground floor location */
+    public static final int LOWEST_FLOOR = 1;
+    
+    /** Represents the mail room location */
+    public static final int MAILROOM_LOCATION = 1;
+
+}
